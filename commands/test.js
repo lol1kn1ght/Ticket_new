a18f7fd87d90a4521b65d6fc0cbd53323b16b745
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-09-18 23:55:41 +0300

    тест

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,18 @@
-const { Command_template } = require("../config/templates");
+const {Command_template} = require("../config/templates");
 
 class Command extends Command_template {
   constructor(args, interaction) {
     super(interaction);
     Object.assign(this, args);
+
+    this.options = {
+      permissions: ["ADMINISTRATOR"],
+      custom_perms: ["OWNER"],
+      slash: {
+        name: "test",
+        description: "Комадна для тестов [BOT_OWNER]"
+      }
+    };
   }
 
   async execute() {
