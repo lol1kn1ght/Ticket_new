e64503e5c0957571e290423271388b8ecd27aef6
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-09-18 23:52:31 +0300

    Добавил отрицательный ембед, доделал везде опции для сообщений

--- config/templates/Command_template.js
+++ config/templates/Command_template.js
@@ -1,18 +1,41 @@
-const { MessageEmbed } = require("discord.js");
+const {MessageEmbed} = require("discord.js");
 
 module.exports = class Command_template {
   constructor(interaction) {
     this.interaction = interaction;
   }
 
-  msg(embed_text) {
-    let embed = new MessageEmbed()
+  msg(embed_text, options) {
+    let embedTrue = new MessageEmbed()
       .setAuthor(
         this.interaction.member.user.tag,
-        this.interaction.member.user.displayAvatarURL({ dynamic: true })
+        this.interaction.member.user.displayAvatarURL({dynamic: true})
       )
-      .setDescription(embed_text);
+      .setDescription(embed_text)
+      .setColor(this.config.colorEmbed)
+      .setTimestamp();
 
-    return this.interaction.reply({ embeds: [embed] });
+    return this._send(embedTrue, options);
+  }
+
+  msgFalse(embed_text, options) {
+    let embedFalse = new MessageEmbed()
+      .setAuthor(
+        this.interaction.member.user.tag,
+        this.interaction.member.user.displayAvatarURL({dynamic: true})
+      )
+      .setDescription(embed_text)
+      .setColor(this.config.colorEmbedFalse)
+      .setTimestamp();
+
+    return this._send(embedFalse, options);
+  }
+
+  _send(embed, options) {
+    try {
+      this.interaction.reply(Object.assign({embeds: [embed]}, options));
+    } catch (e) {
+      this.interaction.reply({embeds: [embed]});
+    }
   }
 };
