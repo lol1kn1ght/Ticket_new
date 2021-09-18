cb256d348ff3799c1f43b6639db8c1026d96220f
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-09-18 23:54:53 +0300

    Добавлена начальная версия прав доступа

--- events/interactionCreate COMMANDS.js
+++ events/interactionCreate COMMANDS.js
@@ -1,4 +1,4 @@
-module.exports = function (args, interaction) {
+module.exports = function(args, interaction) {
   class Event {
     constructor(args) {
       Object.assign(this, args);
@@ -12,7 +12,37 @@ module.exports = function (args, interaction) {
 
       if (!Command) return;
       let command = new Command(args, this.interaction);
+      let options = command.options;
+
+      if (options.permissions && options.permissions[0]) {
+        console.log("есть");
+        let member_perms = options.permissions.filter(permission =>
+          this.interaction.member.permissions.has(permission.toUpperCase())
+        );
+
+        if (!member_perms[0]) {
+          console.log("неут");
+          if (options.custom_perms && options.custom_perms.includes("OWNER")) {
+            if (this.config.owner !== interaction.member.id)
+              return this.noPermissions();
+          } else return this.noPermissions();
+        }
+      }
+
+      if (
+        options.channels &&
+        !options.channels.includes(this.interaction.channelId)
+      )
+        return this.noPermissions();
+
       command.execute();
+    }
+
+    noPermissions() {
+      this.interaction.reply({
+        content: "У вас недостаточно прав для использования этой команды!",
+        ephemeral: true
+      });
     }
   }
 
