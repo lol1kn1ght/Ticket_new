619c45e15f968f9074d989b11f4abc733b6dbb98
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-09-19 17:28:21 +0300

    Доделана команда.

--- commands/create-panel.js
+++ commands/create-panel.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 const {Command_template} = require("../config/templates");
 
 class Command extends Command_template {
@@ -7,10 +8,10 @@ class Command extends Command_template {
 
     this.options = {
       permissions: ["ADMINISTRATOR"],
-      channels: ["742776483944071169"],
+      channels: ["707162375127826504"],
       custom_perms: [],
       slash: {
-        name: "create-panel",
+        name: "создать-панель",
         description: "Создать панель. [ADMINISTRATOR]",
         options: [
           {
@@ -21,8 +22,14 @@ class Command extends Command_template {
           },
           {
             name: "канал",
-            description: "айди канала для панели.",
-            type: 3,
+            description: "айди канала для новой панели.",
+            type: 7,
+            required: true
+          },
+          {
+            name: "следящий",
+            description: "роль следящего для новой панели.",
+            type: 8,
             required: true
           }
         ]
@@ -31,6 +38,8 @@ class Command extends Command_template {
   }
 
   async execute() {
+    this.db = this.mongo.db(this.interaction.guild.id);
+
     let panel_name = this.command_args.filter(
       option => option.name === "название"
     )[0]?.value;
@@ -42,6 +51,58 @@ class Command extends Command_template {
     let panel_channel = this.interaction.guild.channels.cache.get(args_channel);
     if (!panel_channel)
       return this.msgFalseH("Вы указали несуществующий канал для панели!");
+
+    let moderator_role = this.command_args.filter(
+      option => option.name === "следящий"
+    )[0]?.role;
+    if (!moderator_role) return this.msgFalseH("Вы не указали роль следящего!");
+
+    let panel_embed = new Discord.MessageEmbed()
+      .setTitle(panel_name)
+      .setDescription("Для создания нового тикета нажмите на кнопку ниже.")
+      .setColor(this.config.colorEmbed);
+    let row = new Discord.MessageActionRow();
+
+    let buttons = [
+      new Discord.MessageButton({
+        type: "BUTTON",
+        label: "Создать тикет",
+        customId: "create_ticket",
+        style: 2,
+        disabled: false
+      })
+    ];
+
+    row.addComponents(...buttons);
+
+    let panels_db = this.db.collection("tickets_panels");
+    let panels_data = await panels_db.find().toArray();
+
+    let panel_id = this.f.random(0, 10000);
+
+    while (panels_data.filter(panel => panel.panel_id === panel_id)[0]) {
+      panel_id = this.f.random(0, 10000);
+    }
+
+    try {
+      let panel_message = await panel_channel.send({
+        embeds: [panel_embed],
+        components: [row]
+      });
+      this.panel_message = panel_message;
+    } catch (e) {
+      this.msgFalseH("При формировании сообщения панели возникла ошибка!");
+    }
+    let new_panel = {
+      panel_name: panel_name,
+      tickets_channel: panel_channel.id,
+      panel_message: this.panel_message.id,
+      panel_id: panel_id,
+      moderator_roles: [moderator_role.id]
+    };
+
+    panels_db.insertOne(new_panel);
+    this.msgH("Вы успешно создали новую панель для тикетов!");
   }
 }
 
