1ce2bbee970f7d2a5c131bffb94aed706032211d
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-09-21 14:04:40 +0300

    Поменял цепочку отправки ембеда + добавил редактировние + и - ембеда

--- config/templates/Command_template.js
+++ config/templates/Command_template.js
@@ -6,6 +6,60 @@ module.exports = class Command_template {
   }
 
   msg(embed_text, options) {
+    let embedTrue = this._true_embed(embed_text);
+
+    return this._send(embedTrue, options);
+  }
+
+  msgH(embed_text, options = {}) {
+    options.ephemeral = true;
+    return this.msg(embed_text, options);
+  }
+
+  msgE(embed_text, options = {}) {
+    if (this.interaction.ephemeral || !this.interaction.replied) {
+      this.msg(embed_text, options);
+      console.log("1");
+      return;
+    }
+
+    let embedTrue = this._true_embed(embed_text, options);
+
+    options.embeds = [embedTrue];
+    try {
+      return this.interaction.editReply(options);
+    } catch (e) {
+      return this.interaction.editReply({embeds: [embedTrue]});
+    }
+  }
+
+  msgFalse(embed_text, options) {
+    let embedFalse = this._false_embed(embed_text);
+
+    return this._send(embedFalse, options);
+  }
+
+  msgFalseH(embed_text, options = {}) {
+    options.ephemeral = true;
+    return this.msgFalse(embed_text, options);
+  }
+
+  msgFalseE(embed_text, options = {}) {
+    if (this.interaction.ephemeral || !this.interaction.replied)
+      return this.msgFalse(embed_text, options);
+
+    let embedTrue = this._false_embed(embed_text, options);
+
+    try {
+      return this.interaction.editReply(embedTrue, options);
+    } catch (e) {
+      return this.interaction.editReply(embedTrue);
+    }
+  }
+
+  _true_embed(embed_text) {
+    if (!embed_text) throw new Error("No embed text");
+
     let embedTrue = new MessageEmbed()
       .setAuthor(
         this.interaction.member.user.tag,
@@ -15,15 +69,12 @@ module.exports = class Command_template {
       .setColor(this.config.colorEmbed)
       .setTimestamp();
 
-    return this._send(embedTrue, options);
+    return embedTrue;
   }
 
-  msgH(embed_text, options = {}) {
-    options.ephemeral = true;
-    this.msg(embed_text, options);
-  }
+  _false_embed(embed_text) {
+    if (!embed_text) throw new Error("No embed text");
 
-  msgFalse(embed_text, options) {
     let embedFalse = new MessageEmbed()
       .setAuthor(
         this.interaction.member.user.tag,
@@ -33,19 +84,27 @@ module.exports = class Command_template {
       .setColor(this.config.colorEmbedFalse)
       .setTimestamp();
 
-    return this._send(embedFalse, options);
+    return embedFalse;
   }
-
-  msgFalseH(embed_text, options = {}) {
-    options.ephemeral = true;
-    this.msgFalse(embed_text, options);
-  }
-
   _send(embed, options) {
     try {
-      this.interaction.reply(Object.assign({embeds: [embed]}, options));
+      if (!this.interaction.replied) {
+        return this.interaction.reply(
+          Object.assign({embeds: [embed]}, options)
+        );
+      }
+      if (this.interaction.replied) {
+        return this.interaction.followUp(
+          Object.assign({embeds: [embed]}, options)
+        );
+      }
     } catch (e) {
-      this.interaction.reply({embeds: [embed]});
+      if (!this.interaction.replied) {
+        return this.interaction.reply(Object.assign({embeds: [embed]}));
+      }
+      if (this.interaction.replied) {
+        return this.interaction.followUp(Object.assign({embeds: [embed]}));
+      }
     }
   }
 };
